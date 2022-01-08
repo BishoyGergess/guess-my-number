8f7577ab1980e294637ac1a18844ac2ff18901b5
Bishoy Gerges <bishoyanwar@outlook.com>
2022-01-07 23:24:13 -0500

    Update main.js
    Fixed some game logic, check the console for the correct number 
    Changed Score logic
    The random number is generated and doesn't change during the session 
    Interactive the message
    Reload button works 
    Still not finished project

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
 'use strict';
-//document.querySelector('.message').textContent = 'Fuck you';
-document.querySelector('.message').textContent;
-document.querySelector('.message').textContent = '😂😘 Correct Number!';
-document.querySelector('.number').textContent = mathRandomInt();
-document.querySelector('.score').textContent = 10;
-//var z = document.querySelector('.guess').value;
-document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
-  //console.log(z);
-});
-//var z = document.querySelector('.guess').value;
-function mathRandomInt() {
-  let x = Math.floor(Math.random() * 20);
+
+let x = 0;
+const randomNum = function () {
   if (x === 0) {
-    return Math.floor(Math.random() * 20);
+    //+1 to make 20 possible and 0 impossible
+    return (x = Math.floor(Math.random() * 20 + 1));
   } else {
     return x;
   }
-}
+};
+var addNumber = function (num) {
+  return num + 1;
+};
+console.log(randomNum());
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '😒😒 No Number!';
+  } else {
+    if (guess == randomNum()) {
+      document.querySelector('.message').textContent = '😂😘 Correct Number!';
+      document.querySelector('.number').textContent = randomNum();
+      let scoreTex = document.querySelector('.score').textContent;
+      let ScoreNum = Number(scoreTex);
+      document.querySelector('.score').textContent = String(
+        addNumber(ScoreNum)
+      );
+      document.body.style.backgroundColor = 'green';
+    } else {
+      if (guess < randomNum()) {
+        document.querySelector('.message').textContent = '🔞 Too Short!';
+      } else {
+        if (guess > randomNum()) {
+          document.querySelector('.message').textContent = '🔞 Too long!';
+        }
+      }
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  location.reload();
+});
